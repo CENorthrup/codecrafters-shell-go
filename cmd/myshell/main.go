f5d9ecd2ae0c20e2b3beda6daa68cc83585cc79f
Clayton Northrup <ce_acct@pm.me>
2025-01-06 13:11:45 -0500

    echo builtin

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -16,7 +16,7 @@ func main() {
 		input, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
-			os.Exit(1)
+			exitCommand("0")
 		}
 
 		input = strings.TrimSpace(input)
@@ -26,31 +26,45 @@ func main() {
 		}
 
 		cmd := inputParts[0]
-		var args []string
-		if len(inputParts) > 1 {
-			args = inputParts[1:]
-		}
+		rawArgs, argsFound := strings.CutPrefix(input, cmd)
+		cleanArgs := strings.TrimSpace(rawArgs)
 
 		switch cmd {
 		case "exit":
-			if len(args) == 0 {
-				args[0] = "1"
+			// fmt.Fprintf(os.Stdout, "..................%s\n", cmd)
+			os.Exit(0)
+			if !argsFound {
+				// fmt.Println("................... no args found")
+				cleanArgs = "1"
 				fmt.Fprintln(os.Stderr, "Error: No exit code provided. Using default exit code 1")
 			}
-			exitCommand(args)
+			exitCommand(cleanArgs)
+		case "echo":
+			// fmt.Fprintf(os.Stdout, "..................%s\n", cmd)
 
+			if !argsFound {
+				cleanArgs = ""
+			}
+			echoCommand(cleanArgs)
 		default:
 			fmt.Fprintln(os.Stderr, cmd+": command not found")
 		}
 	}
 }
 
-func exitCommand(args []string) {
-	code, err := strconv.Atoi(args[0])
+func exitCommand(args string) {
+	exitCode, err := strconv.Atoi(args)
+	// fmt.Printf("........................err: %s", err)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error: invalid syntax. Using default exit code 1")
-		code = 1
+		exitCode = 1
 	}
-	fmt.Fprintf(os.Stdout, "Exiting with code %d", code)
-	os.Exit(code)
+	// fmt.Fprintf(os.Stdout, "Exiting with code %d", exitCode)
+	// fmt.Fprint(os.Stdout, "$ ")
+	os.Exit(exitCode)
+}
+
+func echoCommand(args string) {
+	echoString := strings.TrimSpace(args)
+	fmt.Fprintln(os.Stdout, echoString)
 }
