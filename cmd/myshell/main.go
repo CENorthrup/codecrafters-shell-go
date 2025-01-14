019b0e5cf474e0cebae8cc72fc3d6ac4221603dd
Clayton Northrup <ce_acct@pm.me>
2025-01-14 11:19:13 -0500

    IP1: run a program; refactor

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
-	"path/filepath"
 	"strconv"
 	"strings"
 )
@@ -25,35 +24,97 @@ func main() {
 			continue
 		}
 
-		cmd, args, argsExist := parseInput(input)
+		cmd := parseCommand(input)
+		args, argsExist := parseArgs(cmd, input)
 
-		switch cmd {
-		case "exit":
-			if argsExist {
-				exitCommand(args)
-			} else {
-				fmt.Fprintf(os.Stderr, "Error: No exit code provided. Using default exit code 1\n")
-				exitCommand(1)
-			}
-		case "echo":
-			echoCommand(args)
-		case "type":
-			typeCommand(args)
+		switch cmdType, _ := checkCmdType(cmd); cmdType {
+		case "builtin":
+			runBuiltinCommand(cmd, args, argsExist)
+			// TODO: add alias case
+		case "executable":
+
+			runExternalProgram(cmd, args)
 		default:
-			if cmdPath, cmdExists := checkExecutables(args); cmdExists {
-				execProgram(cmdPath, args)
-			} else {
-				fmt.Fprintf(os.Stderr, "%s: command not found\n", cmd)
-			}
+			fmt.Fprintf(os.Stderr, "%s: command not found\n", cmd)
 		}
 	}
 }
 
-func parseInput(input string) (cmd string, args string, argsExist bool) {
+func parseCommand(input string) string {
 	inputParts := strings.Fields(input)
-	cmd = inputParts[0]
+	return inputParts[0]
+}
+
+func parseArgs(cmd string, input string) (args string, argsExist bool) {
 	rawArgs, argsFound := strings.CutPrefix(input, cmd)
-	return cmd, strings.TrimSpace(rawArgs), argsFound
+	return strings.TrimSpace(rawArgs), argsFound
+}
+
+func checkCmdType(cmd string) (cmdType string, cmdArgs string) {
+	builtin := map[string]struct{}{
+		"echo": {},
+		"exit": {},
+		"type": {},
+	}
+	if _, exists := builtin[cmd]; exists {
+		return "builtin", ""
+	}
+	if cmdPath := checkExecutable(cmd); cmdPath != "" {
+		return "executable", cmdPath
+	}
+	return "", ""
+}
+
+func checkExecutable(args string) string {
+	if cmdPath, err := exec.LookPath(args); err != nil {
+		fmt.Fprintf(os.Stderr, "%s: not found\n", args)
+		return ""
+	} else {
+		return cmdPath
+	}
+}
+
+func runExternalProgram(cmd string, args string) {
+	argSlice := strings.Fields(args)
+	fmt.Fprintf(os.Stderr, "Program was passed %d args (including program name).\n", len(argSlice)+1)
+	fmt.Fprintf(os.Stderr, "Arg #0 (program name): %s\n", cmd)
+	for i, arg := range argSlice {
+		fmt.Fprintf(os.Stderr, "Arg #%d: %s\n", i+1, arg)
+	}
+	cmdPath := checkExecutable(cmd)
+	if cmdPath == "" {
+		fmt.Fprintf(os.Stderr, "Error: Path to command: %s not found", cmd)
+		return
+	}
+	command := exec.Command(cmdPath, argSlice...)
+
+	out, err := command.Output()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: Unable to run program: %s\n", err)
+		return
+	}
+	fmt.Fprintf(os.Stdout, "Program Signature: %s\n", strings.TrimSpace(string(out)))
+}
+
+func runBuiltinCommand(cmd string, args string, argsExist bool) {
+	switch cmd {
+	case "exit":
+		if argsExist {
+			exitCommand(args)
+		} else {
+			fmt.Fprintf(os.Stderr, "Error: No exit code provided. Using default exit code 1\n")
+			exitCommand(1)
+		}
+	case "echo":
+		echoCommand(args)
+	case "type":
+		typeCommand(args)
+	}
+}
+
+// TODO: add aliasCommand()
+func echoCommand(args string) {
+	fmt.Fprintln(os.Stdout, args)
 }
 
 func exitCommand[T string | int](arg T) {
@@ -73,66 +134,12 @@ func exitCommand[T string | int](arg T) {
 	os.Exit(exitCode)
 }
 
-func echoCommand(args string) {
-	fmt.Fprintln(os.Stdout, args)
-}
-
-func typeCommand(args string) {
-	builtin := map[string]struct{}{
-		"echo": {},
-		"exit": {},
-		"type": {},
+func typeCommand(cmd string) {
+	switch cmdType, cmdArgs := checkCmdType(cmd); cmdType {
+	case "builtin":
+		fmt.Fprintf(os.Stdout, "%s is a shell builtin\n", cmd)
+		// TODO: add alias case
+	case "executable":
+		fmt.Fprintf(os.Stdout, "%s is %s\n", cmd, cmdArgs)
 	}
-	if _, exists := builtin[args]; exists {
-		fmt.Printf("%s is a shell builtin\n", args)
-		return
-	}
-	if fullPath, found := checkExecutables(args); found {
-		fmt.Printf("%s is %s\n", args, fullPath)
-		return
-	}
-	fmt.Fprintf(os.Stderr, "%s: not found\n", args)
-}
-
-func checkExecutables(args string) (string, bool) {
-	// cmdPath, err := exec.LookPath(args)
-	// if err != nil {
-	//   fmt.Fprintf(os.Stderr, "Error: Unable to locate executable on PATH: %v\n", err)
-	//   return "", false
-	// }
-	// fullPath := filepath.Join(cmdPath, args)
-	// return fullPath, true
-
-	pathEnv := os.Getenv("PATH")
-	pathEntries := strings.Split(pathEnv, ":")
-
-	for _, dir := range pathEntries {
-		dirContents, err := os.ReadDir(dir)
-		if err != nil {
-			if os.IsNotExist(err) {
-				continue
-			}
-			fmt.Fprintf(os.Stderr, "Error: Unable to access or read PATH directory %s: %v\n", dir, err)
-			continue
-		}
-
-		for _, dirEntry := range dirContents {
-			if dirEntry.Name() == args {
-				fullPath := filepath.Join(dir, dirEntry.Name())
-				dirEntryInfo, err := dirEntry.Info()
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "Error: Unable to retrieve file info for %s: %v\n", dirEntry.Name(), err)
-					return "", false
-				}
-				if dirEntryInfo.Mode()&0111 != 0 {
-					return fullPath, true
-				}
-			}
-		}
-	}
-	return "", false
-}
-
-func execProgram(cmdPath string, args string) {
-	exec.Command(cmdPath, args)
 }
